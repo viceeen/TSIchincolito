ede81dfc5894bc78af17b63e80234b5d94298ae3
vicente <vicentejeldes15@gmail.com>
2024-09-26 17:05:04 -0300

    nuevo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-custom.min.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <title>Inicio</title>
+    <nav class="navbar bg-success navbar-expand-lg ">
+     <div class="container-fluid">
+    
+        <a class="navbar-brand" href="#">
+        Vivero
+        </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="text-light nav-link active align-items-center d-flex" aria-current="page" href="#">
+          <span class="material-icons ">
+          psychiatry
+                      </span> 
+          Plantas
+            </a>
+        </li>
+        <li class="nav-item">
+          <a class="text-light nav-link active align-items-center d-flex" aria-current="page" href="#">Plantas de interior</a>
+        </li>
+        <li class="nav-item">
+          <a class="text-light nav-link active align-items-center d-flex" aria-current="page" href="#">
+            Proveedores
+            </a>
+        </li>
+        
+
+        
+      </ul>
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item m-1">
+            <a class="text-light nav-link align-items-center d-flex" href="#">
+            <span class="material-icons ">
+                        shopping_cart
+                      </span> 
+            </a>
+            </li>
+        </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</head>
+<body>
+
+<div class="container mt-4">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Planta 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Planta 1</h5>
+                        <p class="card-text">Descripción breve de la planta </p>
+                        <div class="d-flex justify-content-between">
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                            <a href="#" class="btn btn-secondary">Editar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Planta 2" >
+                    <div class="card-body">
+                        <h5 class="card-title">Planta 2</h5>
+                        <p class="card-text">Descripción breve de la planta </p>
+                        <div class="d-flex justify-content-between">
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                            <a href="#" class="btn btn-secondary">Editar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Planta 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Planta 3</h5>
+                        <p class="card-text">Descripción breve de la planta </p>
+                        <div class="d-flex justify-content-between">
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                            <a href="#" class="btn btn-secondary">Editar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Planta 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Planta 2</h5>
+                        <p class="card-text">Descripción breve de la planta </p>
+                        <div class="d-flex justify-content-between">
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                            <a href="#" class="btn btn-secondary">Editar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Planta 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Planta 2</h5>
+                        <p class="card-text">Descripción breve de la planta</p>
+                        <div class="d-flex justify-content-between">
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                            <a href="#" class="btn btn-secondary">Editar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+</body>
+</html>
